f721966013a41806a2aaa8a9ccf21584d92b1acf
Damien Regad <dregad@mantisbt.org>
2019-09-04 00:56:51 +0200

    Coding guidelines

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -2,10 +2,13 @@
 auth_reauthenticate();
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
 form_security_validate( 'GaugeSupport_config' );
-$f_GaugeSupport_excl_status			= gpc_get_string_array('excl_status');
-$f_GaugeSupport_incl_severity		= gpc_get_string_array('incl_severity');
-$f_GaugeSupport_excl_resolution		= gpc_get_string_array('excl_resolution');
-plugin_config_set('gaugesupport_excl_status'			, implode(",",$f_GaugeSupport_excl_status)	  );
-plugin_config_set('gaugesupport_incl_severity'			, implode(",",$f_GaugeSupport_incl_severity)  );
-plugin_config_set('gaugesupport_excl_resolution'		, implode(",",$f_GaugeSupport_excl_resolution));
-print_successful_redirect( plugin_page( 'config',TRUE ) );
+
+$f_excl_status = gpc_get_string_array( 'excl_status' );
+$f_incl_severity = gpc_get_string_array( 'incl_severity' );
+$f_excl_resolution = gpc_get_string_array( 'excl_resolution' );
+
+plugin_config_set('gaugesupport_excl_status', implode( ",", $f_excl_status) );
+plugin_config_set('gaugesupport_incl_severity', implode( ",", $f_incl_severity) );
+plugin_config_set('gaugesupport_excl_resolution', implode( ",", $f_excl_resolution) );
+
+print_successful_redirect( plugin_page( 'config', true ) );
