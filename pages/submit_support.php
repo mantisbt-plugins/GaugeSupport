4bc9d8a689e67f22b4229e1cf8485949a2aa3783
Damien Regad <dregad@mantisbt.org>
2019-09-04 01:00:13 +0200

    Coding guidelines

--- pages/submit_support.php
+++ pages/submit_support.php
@@ -1,9 +1,19 @@
 <?php
 form_security_validate( 'GaugeSupport_submit_vote' );
 
-$bugId = gpc_get_int('bugid');
-$stance = gpc_get_int('stance');
-$dbtable = plugin_table("support_data","GaugeSupport");
-$dbquery = "INSERT INTO {$dbtable} (bugid, userid, rating) VALUES (".db_param().",".db_param().",".db_param().") ON DUPLICATE KEY UPDATE rating = ".db_param();
-$dboutput = db_query_bound($dbquery, array($bugId, current_user_get_field("id"), $stance, $stance));
-print_successful_redirect( 'view.php?id=' . $bugId );
+$f_bug_id = gpc_get_int( 'bugid' );
+$f_stance = gpc_get_int( 'stance' );
+
+$t_table = plugin_table( 'support_data', 'GaugeSupport' );
+$t_query = "INSERT INTO {$t_table} (bugid, userid, rating) 
+	VALUES (" . db_param() . "," . db_param() . "," . db_param() . ") 
+	ON DUPLICATE KEY UPDATE rating = " . db_param();
+$t_param = array(
+	$f_bug_id,
+	current_user_get_field( "id" ),
+	$f_stance,
+	$f_stance
+);
+$t_result = db_query_bound( $t_query, $t_param );
+
+print_successful_redirect( 'view.php?id=' . $f_bug_id );
