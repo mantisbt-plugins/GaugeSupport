3c990ac0c2ea7b8818b0fd2cc4b4b4c04cbd0dd2
Damien Regad <dregad@mantisbt.org>
2019-09-05 22:46:14 +0200

    Data retrieval moved to GaugeSupport::getRatings()
    The same code was duplicated in Rankings page and Excel export script.

--- GaugeSupport.php
+++ GaugeSupport.php
@@ -62,4 +62,100 @@ class GaugeSupportPlugin extends MantisPlugin {
 		);
 	}
 
+	/**
+	 * Retrieve aggregated ratings from the database.
+	 *
+	 * @return array
+	 */
+	function getRatings() {
+		# Build where clause
+		$t_where = array();
+		$t_param = array();
+
+		# Project ID filter
+		$t_project_id = helper_get_current_project();
+		if( $t_project_id != 0 ) {
+			$t_where[] = 'b.project_id = ' . db_param();
+			$t_param[] = $t_project_id;
+		}
+
+		# Config filters
+		$t_excl_status = plugin_config_get( 'gaugesupport_excl_status' );
+		$t_excl_resolution = plugin_config_get( 'gaugesupport_excl_resolution' );
+		
+		$t_where[] = "b.status NOT IN ( {$t_excl_status} )";
+		$t_where[] = "b.resolution NOT IN ( {$t_excl_resolution} )";
+
+		$t_where_clause = 'WHERE ' . implode( ' AND ', $t_where );
+
+		# Retrieve rankings from the database
+		$t_ratings_table = plugin_table( 'support_data' );
+		$t_bug_table = db_get_table( 'bug' );
+
+		$t_query = "SELECT
+				max(sd.bugid) as bugid,
+				count(sd.rating) as no_of_ratings,
+				sum(sd.rating) as sum_of_ratings,
+				avg(sd.rating) as avg_rating,
+				max(sd.rating) as highest_rating,
+				min(sd.rating) as lowest_rating,
+				IFNULL(bm2_count,0) AS bm2_count,
+				IFNULL(bm2_sum,0) AS bm2_sum,
+				IFNULL(bm1_count,0) AS bm1_count,
+				IFNULL(bm1_sum,0) AS bm1_sum,
+				IFNULL(b2_count,0) AS b2_count,
+				IFNULL(b2_sum,0) AS b2_sum,
+				IFNULL(b1_count,0) AS b1_count,
+				IFNULL(b1_sum,0) AS b1_sum
+			FROM {$t_ratings_table} sd
+			INNER JOIN {$t_bug_table} b ON sd.bugid = b.id
+			LEFT OUTER JOIN (
+					SELECT bugid, count(rating) as bm2_count, sum(rating) as bm2_sum 
+					FROM {$t_ratings_table} 
+					GROUP BY bugid, rating HAVING rating = -2) bm2 
+				ON sd.bugid = bm2.bugid
+			LEFT OUTER JOIN (
+					SELECT bugid, count(rating) as bm1_count, sum(rating) as bm1_sum 
+					FROM {$t_ratings_table} 
+					GROUP BY bugid, rating 
+					HAVING rating = -1) bm1 
+				ON sd.bugid = bm1.bugid
+			LEFT OUTER JOIN (
+					SELECT bugid, count(rating) as b2_count, sum(rating) as b2_sum 
+					FROM {$t_ratings_table} 
+					GROUP BY bugid, rating HAVING rating = 2) b2 
+				ON sd.bugid = b2.bugid
+			LEFT OUTER JOIN (
+					SELECT bugid, count(rating) as b1_count, sum(rating) as b1_sum 
+					FROM {$t_ratings_table} 
+					GROUP BY bugid, rating HAVING rating = 1) b1 
+				ON sd.bugid = b1.bugid
+			{$t_where_clause}
+			GROUP BY sd.bugid, bm2_count, bm2_sum, bm1_count, bm1_sum, b2_count, b2_sum, b1_count, b1_sum
+			ORDER BY sum(sd.rating) DESC";
+		$t_result = db_query( $t_query, $t_param );
+
+		# Store rankings in an array
+		$t_data = array();
+		
+		while( $t_row = db_fetch_array( $t_result ) ) {
+			$t_bug_id = intval( $t_row['bugid'] );
+
+			$t_data[$t_bug_id] = array(
+				'ratings' => array(
+					-2 => array('count' => $t_row['bm2_count'], 'sum' => $t_row['bm2_sum']),
+					-1 => array('count' => $t_row['bm1_count'], 'sum' => $t_row['bm1_sum']),
+					+1 => array('count' => $t_row['b1_count'], 'sum' => $t_row['b1_sum']),
+					+2 => array('count' => $t_row['b2_count'], 'sum' => $t_row['b2_sum']),
+				),
+				'no_of_ratings' => $t_row['no_of_ratings'],
+				'sum_of_ratings' => $t_row['sum_of_ratings'],
+				'avg_rating' => $t_row['avg_rating'],
+				'highest_rating' => $t_row['highest_rating'],
+				'lowest_rating' => $t_row['lowest_rating'],
+			);
+		}
+		
+		return $t_data;
+	}
 }
--- pages/issue_ranking.php
+++ pages/issue_ranking.php
@@ -1,70 +1,6 @@
 <?php
-	$project['name'] = project_get_name(helper_get_current_project()); // fucking PHP doesn't accept static variables initialized by functions x_x
-	$project['id'] = helper_get_current_project();
+$t_ratings = plugin_get()->getRatings();
 
-	// craft WHERE
-	$where_clause = $project['id'] > 0 ? "WHERE b.project_id = ".$project['id'] : "";
-	$skipresolution = plugin_config_get( 'gaugesupport_excl_resolution' );
-	$skipThese1 = "b.resolution NOT IN (" . implode(",", array($skipresolution)) . ")";
-
-	$skipstatus = config_get( 'plugin_GaugeSupport_gaugesupport_excl_status' );
-	$skipThese2 = "b.status NOT IN (" . implode(",", array($skipstatus)) . ")";
-	
-	if(strlen($where_clause) < 1) {
-		$where_clause = "WHERE {$skipThese1} AND {$skipThese2} " ;
-	} else {
-		$where_clause .= " AND {$skipThese1} AND {$skipThese2} " ;
-	}
-	//// fetch collected data from DB
-	$plugin_table = plugin_table("support_data");
-	$bug_table = db_get_table('bug');
-	$dbquery = "SELECT
-		max(sd.bugid) as bugid,
-		count(sd.rating) as no_of_ratings,
-		sum(sd.rating) as sum_of_ratings,
-		avg(sd.rating) as avg_rating,
-		max(sd.rating) as highest_rating,
-		min(sd.rating) as lowest_rating,
-		IFNULL(bm2_count,0) AS bm2_count,
-		IFNULL(bm2_sum,0) AS bm2_sum,
-		IFNULL(bm1_count,0) AS bm1_count,
-		IFNULL(bm1_sum,0) AS bm1_sum,
-		IFNULL(b2_count,0) AS b2_count,
-		IFNULL(b2_sum,0) AS b2_sum,
-		IFNULL(b1_count,0) AS b1_count,
-		IFNULL(b1_sum,0) AS b1_sum
-	FROM {$plugin_table} sd
-	INNER JOIN {$bug_table} b ON sd.bugid = b.id
-	LEFT OUTER JOIN (SELECT bugid, count(rating) as bm2_count, sum(rating) as bm2_sum FROM {$plugin_table} GROUP BY bugid, rating HAVING rating = -2) bm2 ON sd.bugid = bm2.bugid
-	LEFT OUTER JOIN (SELECT bugid, count(rating) as bm1_count, sum(rating) as bm1_sum FROM {$plugin_table} GROUP BY bugid, rating HAVING rating = -1) bm1 ON sd.bugid = bm1.bugid
-	LEFT OUTER JOIN (SELECT bugid, count(rating) as b2_count, sum(rating) as b2_sum FROM {$plugin_table} GROUP BY bugid, rating HAVING rating = 2) b2 ON sd.bugid = b2.bugid
-	LEFT OUTER JOIN (SELECT bugid, count(rating) as b1_count, sum(rating) as b1_sum FROM {$plugin_table} GROUP BY bugid, rating HAVING rating = 1) b1 ON sd.bugid = b1.bugid
-	{$where_clause}
-	GROUP BY sd.bugid, bm2_count, bm2_sum, bm1_count, bm1_sum, b2_count, b2_sum, b1_count, b1_sum
-	ORDER BY sum(sd.rating) DESC ";
-	// echo "<p>$dbquery</p>";
-	//die();
-	$dboutput = db_query($dbquery);
-
-
-	$resultset = array();
-	// load listable issues into array
-	while($row = db_fetch_array($dboutput)) {
-		$row_bug_id = intval($row['bugid']);
-		$resultset[$row_bug_id] = array();
-		$resultset[$row_bug_id]['ratings'] = array();
-		$resultset[$row_bug_id]['ratings'][-2] = array('count' => $row['bm2_count'], 'sum' => $row['bm2_sum']);
-		$resultset[$row_bug_id]['ratings'][-1] = array('count' => $row['bm1_count'], 'sum' => $row['bm1_sum']);
-		$resultset[$row_bug_id]['ratings'][1] = array('count' => $row['b1_count'], 'sum' => $row['b1_sum']);
-		$resultset[$row_bug_id]['ratings'][2] = array('count' => $row['b2_count'], 'sum' => $row['b2_sum']);
-		$resultset[$row_bug_id]['no_of_ratings'] = $row['no_of_ratings'];
-		$resultset[$row_bug_id]['sum_of_ratings'] = $row['sum_of_ratings'];
-		$resultset[$row_bug_id]['avg_rating'] = $row['avg_rating'];
-		$resultset[$row_bug_id]['highest_rating'] = $row['highest_rating'];
-		$resultset[$row_bug_id]['lowest_rating'] = $row['lowest_rating'];
-	}
-$topic = "Most supported " . $project['name'] . " issues " ;	
-// ==== PAGE GENERATION STARTS HERE ====
 layout_page_header( plugin_lang_get( 'title' ) );
 layout_page_begin( );
 ?>
@@ -83,7 +19,7 @@ layout_page_begin( );
 		<div class="widget-main no-padding">
 
 <?php
-	if( empty( $resultset ) ) {
+	if( empty( $t_ratings ) ) {
 ?>
 			<div class="container-fluid">
 				<br>
@@ -115,7 +51,7 @@ layout_page_begin( );
 						<th><?php echo plugin_lang_get( 'rating_low' ); ?></th>
 					</tr>
 <?php
-foreach($resultset as $bugid => $data) {
+foreach( $t_ratings as $bugid => $data) {
 	$bug = bug_get( $bugid );
 ?>
 					<tr>
