a854b1e41c78228147f45ddee0203541ddc43145
Damien Regad <dregad@mantisbt.org>
2019-09-06 02:02:31 +0200

    Redirect to Manage Plugins page after config update
    Print operation successful message and return to manage_plugin_page.php
    instead of staying on the config page after update.
    
    Fixes #26

--- pages/config_edit.php
+++ pages/config_edit.php
@@ -3,6 +3,10 @@ auth_reauthenticate();
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
 form_security_validate( 'GaugeSupport_config' );
 
+$t_redirect_url = 'manage_plugin_page.php';
+layout_page_header( null, $t_redirect_url );
+layout_page_begin();
+
 $f_excl_status = gpc_get_string_array( 'excl_status' );
 $f_incl_severity = gpc_get_string_array( 'incl_severity' );
 $f_excl_resolution = gpc_get_string_array( 'excl_resolution' );
@@ -11,4 +15,8 @@ plugin_config_set('gaugesupport_excl_status', implode( ",", $f_excl_status) );
 plugin_config_set('gaugesupport_incl_severity', implode( ",", $f_incl_severity) );
 plugin_config_set('gaugesupport_excl_resolution', implode( ",", $f_excl_resolution) );
 
-print_successful_redirect( plugin_page( 'config', true ) );
+html_operation_successful(
+	$t_redirect_url,
+	plugin_lang_get( 'config_updated' )
+);
+layout_page_end();
